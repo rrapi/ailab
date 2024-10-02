30c3bd107c3499f9941aa9a6b80422dec67c777a
rrapi <rrapi.franci@gmail.com>
2024-10-02 16:25:15 +0200

    docker env creation 0:1

--- bin/init_sim.bash
+++ bin/init_sim.bash
@@ -1,9 +1,11 @@
 #!/bin/ba
 
-SIM=init_sim_ur5e.bash
+init_sim_'$1'.bash
 
-if [ "$1" == "panda" ]; then
-  SIM=init_sim_panda.bash
-fi
+# if [ "$1" == "panda" ]; then
+#   SIM=init_sim_panda.bash
+# elif [ "$1" == "ur5e" ]; then
+#   SIM=init_sim_ur5e.bash
+# fi
 
-$SIM
+# $SIM
