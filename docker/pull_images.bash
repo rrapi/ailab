a82a7da48e58cc41ea526dc51a360447de4616de
rrapi <rrapi.franci@gmail.com>
2025-01-27 11:43:31 +0100

    update file

--- docker/pull_images.bash
+++ docker/pull_images.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+IMAGE_VERSION=latest
+
+if [ "$1" != "" ]; then
+  IMAGE_VERSION=$1
+fi
+
+
 docker pull devrt/xserver
-docker pull francirrapi/ros_noetic_gazebo:latest
+docker pull francirrapi/ros_noetic_gazebo:$IMAGE_VERSION
 # docker pull nginx
