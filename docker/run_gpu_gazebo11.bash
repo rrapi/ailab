52c4c70bed94139c8d93a94898846ad5ad63b884
rrapi <rrapi.franci@gmail.com>
2026-05-20 14:38:38 +0200

    update run

--- docker/run_gpu_gazebo11.bash
+++ docker/run_gpu_gazebo11.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# In X11 case, before running docker compose up, you must give local applications permission to connect to your X11 screen: xhost +local:root
+
 export AILAB_DIR=`pwd | gawk '{ print gensub(/\/docker/, "", 1) }'`
 
 DC=dc_g11_nvidia_vnc.yml
